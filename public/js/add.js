a8b860bde626213bb08d92f8d4e67ed9e1273953
Arthur <arthurd800@wp.pl>
2020-03-16 10:56:39 +0100

    finished frontend

---
+++ public/js/add.js
@@ -0,0 +1,36 @@
+const storeForm = document.querySelector('#store-form');
+const storeId = document.querySelector('#store-id');
+const storeAddress = document.querySelector('#store-address');
+
+storeForm.addEventListener('submit',addStore);
+
+async function addStore(e)
+{
+    e.preventDefault();
+    if(storeId.value === '' || storeAddress.value === '')
+    {
+        alert('Please fill in fields');        
+    }
+    const sendBody = {
+        storeId: storeId.value,
+        address: storeAddress.value
+    };
+    try {
+      const res = await fetch('/api/v1/stores',{
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(sendBody)
+      });
+      if(res.status === 400)
+      {
+          throw Error('Store already exists!');
+      }
+      alert('Store successfuly added');
+      window.location.href = '/index.html';
+    } catch (err) {
+        alert(err);
+        return;
+    }
+}
